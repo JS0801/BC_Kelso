3604b5a468f1ccab9a533d8f6ee1d0dd290ca564
JS0801 <jsuthar@oncloudconsulting.ca>
2026-07-20 17:01:48 -0400

    Update from NetSuite (Auto)

--- BC_RGD_AIA_Forms_UE.js
+++ BC_RGD_AIA_Forms_UE.js
@@ -13,6 +13,23 @@ define(['N/ui/serverWidget', 'N/url', 'N/search'], function(ui, url, search) {
       var recordId = context.newRecord.id;
       var sub = current_rec.getValue('subsidiary');
       var bcProj = current_rec.getValue('cseg_bc_project');
+      var custId = current_rec.getValue('entity');
+
+      if (custId == 787592) {
+        var scriptUrl1 = url.resolveScript({
+          scriptId: 'customscript_bc_sl_print_wavier_afs',
+          deploymentId: 'customdeploy_bc_sl_print_wavier_afs'
+        });
+
+
+        var buttonScript1 = "window.open('" + scriptUrl + "&recordid=" + recordId +"', '_blank');";
+
+        form.addButton({
+          id: 'custpage_my_button1',
+          label: 'Print AFS Form',
+          functionName: buttonScript1
+        });
+      }
 
       if (sub == Ralph_G_Degli && bcProj){
         // Add a custom button to the form
